cd03833c42af14a222d3a41da1bfac13a72aeaa4
lzn943362 <411121080@qq.com>
2018-09-24 19:03:44 -0700

    第一次提交

--- Django/Day09/AXF/APP/views.py
+++ Django/Day09/AXF/APP/views.py
@@ -7,12 +7,24 @@ def home(request):
 
     #获取首页数据
     #轮播数据u
-    wheels = MainWheel.objects.all()
-    data = {
-        'wheels': wheels,
-    }
-    return render(request, 'home/home.html', context=data)
+    wheel = MainWheel.objects.all()
+    nav = MainNav.objects.all()
+    mustbuy = MainMustbuy.objects.all()
+    shop = MainShop.objects.all()
+    shop1 = MainShop.objects.get(id=1)
 
+    mainlist = MainShow.objects.all()
+    data = {
+        'wheels': wheel,
+        'navs': nav,
+        'mustbuys': mustbuy,
+        'shop1': shop1,
+        'shop2': shop[1:3],
+        'shop3': shop[3:7],
+        'shop4': shop[7:11],
+        'main_list': mainlist,
+    }
+    return render(request, 'home/home.html', data)
 def cart(request):
     return render(request, 'cart/cart.html')
 
